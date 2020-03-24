0b50932210ddfbcf31411b973e1128fbcf4a85bd
Anders K. Andersen <aka@myself.com>
2020-03-24 14:31:00 +0100

    my empty spring boot application

--- src/main/java/springboot/Main.java
+++ src/main/java/springboot/Main.java
@@ -1,8 +1,67 @@
 package springboot;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello main");
+        System.out.println("starting query maven");
+
+        Main m = new Main();
+        try {
+            m.checkDependencyTree();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
+
+    boolean isWindows = System.getProperty("os.name")
+            .toLowerCase().startsWith("windows");
+
+
+    public void checkDependencyTree() {
+
+        ProcessBuilder processBuilder = new ProcessBuilder();
+
+        if (isWindows) {
+            processBuilder.command("cmd.exe", "/c", "mvn dependency:tree");
+        } else {
+            processBuilder.command("bash", "-c", "mvn dependency:tree");
+        }
+
+        try {
+
+            Process process = processBuilder.start();
+
+            StringBuilder output = new StringBuilder();
+
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(process.getInputStream()));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if(!line.startsWith("[INFO]") || line.contains("Finished at")){
+                    output.append(line).append("\n");
+                }
+            }
+
+            int exitVal = process.waitFor();
+            if (exitVal == 0) {
+                System.out.println("calling maven completed");
+                System.out.println(output);
+                System.exit(0);
+            } else {
+                System.exit(exitVal);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
